611cb59dca7241e4961c84016ca68629ad989573
VVolkov833 <finnish.ru@gmail.com>
2021-09-12 01:23:39 +0300

    make h2 css-dependent on photo

--- forms/entity-add/templates/entities-archive.php
+++ forms/entity-add/templates/entities-archive.php
@@ -1,5 +1,7 @@
 <?php
 
+$imgs_dir = str_replace( ABSPATH, get_site_url() . '/', dirname( __DIR__ ) . '/templates/images/' );
+
 get_header();
 
 ?>
@@ -12,8 +14,10 @@ $wp_query = new WP_Query( [
     'post_type'        => ['clinic', 'doctor'],
     'orderby'          => 'date',
     'order'            => 'DESC',
-    'posts_per_page'   => '7',
-    'post_status'      => ['publish', 'private'] // ++test it
+    'posts_per_page'   => '12',
+    'paged'            => get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1,
+    'post_status'      => ['publish', 'private'], // ++test it
+    
 ]);
 
 if ( $wp_query->have_posts() ) {
@@ -24,11 +28,16 @@ if ( $wp_query->have_posts() ) {
 ?>
 
 <article class="post-<?php the_ID() ?> <?php echo get_post_type() ?> type-<?php echo get_post_type() ?> status-<?php echo get_post_status() ?> entry" itemscope="" itemtype="https://schema.org/CreativeWork">
+
+    <a class="entry-link-cover" rel="bookmark" href="<?php the_permalink(); ?>" title="<?php the_title() ?>"></a>
+
     <header class="entry-header">
-        <!-- badges go here -->
-        <h2 class="entry-title" itemprop="headline">
-            <a rel="bookmark" href="<?php the_permalink() ?>"><?php the_title() ?></a>
-        </h2>
+        <div class="entity-badges">
+            <img loading="lazy" width="23" height="38" src="<?php echo $imgs_dir . 'verified.png' ?>" alt="Verified" title="Verified" />
+            <?php if ( fct_print_meta( 'entity-featured', true ) ) { ?>
+                <img loading="lazy" width="23" height="38" src="<?php echo $imgs_dir . 'featured.png' ?>" alt="Featured" title="Featured" />
+            <?php } ?>
+        </div>
         <?php if ( $back_img = fct_print_meta( 'entity-photo', true )[0] ) { ?>
             <div class="entry-photo">
                 <img loading="lazy" width="100%" height="100%"
@@ -37,6 +46,9 @@ if ( $wp_query->have_posts() ) {
                 />
             </div>
         <?php } ?>
+        <h2 class="entry-title" itemprop="headline">
+            <a rel="bookmark" href="<?php the_permalink() ?>"><?php the_title() ?></a>
+        </h2>
     </header>
     <div class="entry-details">
         <?php if ( $logo = fct_print_meta( 'entity-avatar', true )[0] ) { ?>
@@ -47,13 +59,15 @@ if ( $wp_query->have_posts() ) {
             />
         </div>
         <?php } ?>
-        <p><?php fct_print_meta( 'entity-specialty' ); fct_print_meta( 'entity-geo-city', false, ' in ' ) ?></p>
-        <?php if ( method_exists( 'FCP_Comment_Rate', 'print_rating_summary_short' ) ) { ?>
-            <?php //FCP_Comment_Rate::print_rating_summary_short() ?>
-        <?php } ?>
+        <div class="entity-about">
+            <p>
+                <?php fct_print_meta( 'entity-specialty' ); fct_print_meta( 'entity-geo-city', false, ' in ' ) ?>
+            </p>
+            <?php if ( method_exists( 'FCP_Comment_Rate', 'print_stars_total' ) ) { ?>
+                <?php FCP_Comment_Rate::print_stars_total() ?>
+            <?php } ?>
+        </div>
     </div>
-    <a class="entry-link-cover" rel="bookmark" href="<?php the_permalink(); ?>" title="<?php the_title() ?>">
-    </a>
 
 </article>
 
