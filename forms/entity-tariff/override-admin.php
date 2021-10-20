467f52dac66e512dc134be1ec0dbf0bd3a803731
VVolkov833 <finnish.ru@gmail.com>
2021-10-20 23:41:01 +0300

    non-date values added to tarif end date

--- forms/entity-tariff/override-admin.php
+++ forms/entity-tariff/override-admin.php
@@ -3,4 +3,28 @@
 Modify the values before printing to inputs
 */
 
-$values['entity-tariff-till'] = date( 'd.m.Y', $values['entity-tariff-till'] );
+if ( current_user_can('administrator') ) {
+    if ( !$values['entity-tariff-till'] ) {
+        $values['entity-tariff-till'] = '';
+        return;
+    }
+    $values['entity-tariff-till'] = date( 'd.m.Y', $values['entity-tariff-till'] );
+    return;
+}
+
+if ( $values['entity-tariff-till'] ) {
+
+    if ( $values['entity-tariff-till'] < time() ) {
+        if ( time() - $values['entity-tariff-till'] < 60*60*24 ) {
+            $values['entity-tariff-till'] = 'Ends today';
+            return;
+        }
+        $values['entity-tariff-till'] = 'Not set or ended recently';
+        return;
+    }
+
+    $values['entity-tariff-till'] = date( get_option( 'date_format' ), $values['entity-tariff-till'] );
+    return;
+}
+
+$values['entity-tariff-till'] = 'Not limited'; // 0
--- forms/entity-tariff/process-admin.php
+++ forms/entity-tariff/process-admin.php
@@ -5,12 +5,17 @@ Process meta boxes data
 
 // custom $_POST filters
 
+if ( !current_user_can('administrator') ) { return; }
+
 $_POST['entity-tariff-till'] = trim( $_POST['entity-tariff-till'] );
 
 if (
     !$_POST['entity-tariff-till'] ||
     !preg_match( '/^\d{1,2}\.\d{1,2}\.\d{2,4}$/', $_POST['entity-tariff-till'] )
-) { return; }
+) {
+    $_POST['entity-tariff-till'] = 0;
+    return;
+}
 
 
 $d = DateTime::createFromFormat( 'd.m.y', $_POST['entity-tariff-till'] );
