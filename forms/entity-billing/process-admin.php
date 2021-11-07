1696ad5ff0a64df98b31f17dfed394f3ea214479
VVolkov833 <finnish.ru@gmail.com>
2021-11-07 19:10:47 +0300

    a hole fixed

--- forms/entity-billing/process-admin.php
+++ forms/entity-billing/process-admin.php
@@ -3,4 +3,17 @@
 Process meta boxes data
 */
 
-// ++check if billing method and the clinic belongs to the same user??
+// the billing method must belong to the delegate
+$args = [
+    'author'         => $post->post_author,
+    'post_type'      => 'billing',
+    'p'              => $_POST['entity-billing'],
+    'posts_per_page' => 1,
+    'post_status'      => ['publish', 'private'],
+];
+
+$wp_query = new WP_Query( $args );
+
+if ( !$wp_query->have_posts() ) {
+    $_POST['entity-billing'] = '';
+}
