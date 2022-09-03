44fc3ca673a335bc81c6bb6754a722e90d5a4f88
VVolkov833 <finnish.ru@gmail.com>
2022-09-03 23:03:37 +0300

    sizes added for sake of google

--- forms/entity-add/templates/functions.php
+++ forms/entity-add/templates/functions.php
@@ -106,7 +106,7 @@ function print_contact_button($meta_name, $name, $itemprop = '') {
     ?>
         <div class="fct1-tile-one">
             <a href="<?php echo $prefix ?><?php echo $meta_value ?>"<?php echo $attrs ?>></a>
-            <img src="/wp-content/plugins/fcp-forms/forms/entity-add/templates/imgs/<?php echo $img ?>.svg" class="attachment-full size-full" alt="" loading="lazy">
+            <img width="100" height="80" src="/wp-content/plugins/fcp-forms/forms/entity-add/templates/imgs/<?php echo $img ?>.svg" class="attachment-full size-full" alt="" loading="lazy">
             <p<?php echo $itemprop ? ' itemprop="'.$itemprop.'" content="'.$meta_value.'" ' : '' ?>><?php echo $name ?></p>
         </div>
     <?php
